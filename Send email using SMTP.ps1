747da90d8aae5e0242ffb12b0e8e041fc4956568
Wanderson Silva <ca-scudo@hotmail.com>
2025-02-27 02:35:11 -0300

    Update Send email using SMTP.ps1

--- Send email using SMTP.ps1
+++ Send email using SMTP.ps1
@@ -1,5 +1,6 @@
 #Send email using SMTP
-
+#base de configurações de servidor smtp do Office 365
+#https://support.microsoft.com/pt-br/office/configura%C3%A7%C3%B5es-pop-imap-e-smtp-para-outlook-com-d088b986-291d-42b8-9564-9c414e2aa040
 
 $Username = "e-mail do remetente";
 $Password = "senha";
