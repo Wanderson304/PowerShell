66b1aa659b1036784f523f657b03c968a4a9b0e9
Wanderson Silva <ca-scudo@hotmail.com>
2025-03-02 15:53:24 -0300

    Update Pieces of code.ps1

--- Pieces of code.ps1
+++ Pieces of code.ps1
@@ -6,5 +6,7 @@
         $DomainName = (Get-WMIObject Win32_ComputerSystem).Domain
         Write-Host  "Dominio logado: $DomainName" -ForegroundColor Gree
         Write-Host  "Usuário logado: $Env:UserName"   -ForegroundColor Gree
+        #Essa opção é para caso você queira enumerar algum módulo.
+        #Caso não queira exibir isso é só comentar
         Write-Host  "Versão do módulo: VS.1.0"   -ForegroundColor Gree
 }
