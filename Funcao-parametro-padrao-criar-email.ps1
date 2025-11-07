95da0cbff5ad65f658c4e420e484c247017c0bfe
Wanderson Silva <ca-scudo@hotmail.com>
2025-11-07 02:45:08 -0300

    Enhance comments and add reference link
    Added a reference link and comments for clarity.

--- Funcao-parametro-padrao-criar-email.ps1
+++ Funcao-parametro-padrao-criar-email.ps1
@@ -1,6 +1,9 @@
+#Base:
+#https://www.sharepointdiary.com/2021/11/powershell-function.html
 
 #Exemplo de parametro padrão.
 #Criação de e-mail.
+#Se o dominio não for digitado, o domini padrão sera @vale.com.br
 
 #Criar e-mail
 function Criar-email  {
